f34642d81a1691304db3b4029a80cccb8e9830df
tlcrack <league@WTS5.attlocal.net>
2018-12-23 11:57:04 -0800

    TeaParty, TeaPartyTest

--- src/TeaParty.java
+++ src/TeaParty.java
@@ -3,7 +3,26 @@ public class TeaParty {
 String name;
 boolean isWoman;
 boolean isKnighted;
+
+
+
+	
+
+
     public String welcome(String name, boolean isWoman, boolean isKnighted) {
-return "";
+    String welcomer =  "";
+    if(isWoman==true&&isKnighted==true) {
+    	welcomer = "Hello Lady " + name;
+    }
+    if(isWoman==true&&isKnighted==false) {
+    	welcomer = "Hello Ms. " + name;
+    }
+    if(isWoman==false&&isKnighted==true) {
+    	welcomer = "Hello Sir " + name;
+    }
+    if(isWoman==false&&isKnighted==false) {
+    	welcomer = "Hello Mr. " + name;
+    }
+    	return welcomer;
     }
 }
